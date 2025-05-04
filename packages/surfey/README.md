1a99307f08f029053fb1d8a109493aafd3aff727
Oliver Bartsch <bo@cedev.de>
2025-05-04 12:40:38 +0100

    [TASK] Kickstart extension

---
+++ packages/surfey/README.md
@@ -0,0 +1,11 @@
+# Surfey - Create, manage, and analyze surveys with ease in TYPO3
+
+## Installation
+
+Install this extension via `composer req typo3-incubator/surfey`.
+
+**Note**: This extension is compatible with TYPO3 v13.
+
+## Credits
+
+This extension was created with ❤️ by the Surfcamp #t3rrific team on Fuerteventura in 2025.
---
+++ packages/surfey/ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Surfey',
+    'description' => 'Create, manage, and analyze surveys with ease in TYPO3.',
+    'category' => 'module',
+    'author' => 'TYPO3 Surfcamp Team 4',
+    'author_email' => 'typo3cms@typo3.org',
+    'state' => 'beta',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'TYPO3Incubator\\Surfey\\' => 'Classes/',
+        ]
+    ]
+];
