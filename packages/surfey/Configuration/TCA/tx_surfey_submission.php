aa7754b768b9d025974ad30ffbdd64343451e76e
Oliver Bartsch <bo@cedev.de>
2025-05-05 17:03:07 +0100

    [TASK] Add tx_surfey_submission table

---
+++ packages/surfey/Configuration/TCA/tx_surfey_submission.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:surfey/Resources/Private/Language/locallang_tca.xlf:tx_surfey_submission.title',
+        'label' => 'surfey',
+        'crdate' => 'crdate',
+        'tstamp' => 'tstamp',
+        'groupName' => 'system',
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'default_sortby' => 'crdate',
+        'delete' => 'deleted',
+        'typeicon_classes' => [
+            'default' => 'tx-surfey',
+        ],
+        'enablecolumns' => [
+            'disabled' => 'disabled',
+        ],
+        'rootLevel' => 1,
+        'readOnly' => true,
+        'hideTable' => true,
+    ],
+    'types' => [
+        '1' => [
+            'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                surfey,hash,data,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                disabled,
+        '],
+    ],
+    'palettes' => [
+        'language' => ['showitem' => 'sys_language_uid, l10n_parent'],
+    ],
+    'columns' => [
+        'surfey' => [
+            'label' => 'LLL:EXT:surfey/Resources/Private/Language/locallang_tca.xlf:tx_surfey_submission.columns.surfey.label',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'foreign_table' => 'tx_surfey_definition',
+            ],
+        ],
+        'hash' => [
+            'label' => 'LLL:EXT:surfey/Resources/Private/Language/locallang_tca.xlf:tx_surfey_submission.columns.hash.label',
+            'config' => [
+                'type' => 'input',
+                'eval' => 'trim',
+            ],
+        ],
+        'data' => [
+            'label' => 'LLL:EXT:surfey/Resources/Private/Language/locallang_tca.xlf:tx_surfey_submission.columns.data.label',
+            'config' => [
+                'type' => 'json',
+                'default' => '{}',
+                'readOnly' => true,
+            ]
+        ],
+    ],
+];
