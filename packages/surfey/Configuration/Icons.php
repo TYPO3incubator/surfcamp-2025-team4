2a60680b69a7cdef8ab460f984bda7d8ea4b8a49
Oliver Bartsch <bo@cedev.de>
2025-05-05 16:27:23 +0100

    [TASK] Add tx_survey_definition table

---
+++ packages/surfey/Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'tx-surfey' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:surfey/Resources/Public/Icons/Extension.png',
+    ],
+];
